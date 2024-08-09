d705bd23cda3c63ccbe79922b7232b128b7b8269
Stephen Hodgson <hodgson.designs@gmail.com>
2024-08-08 22:14:59 -0400

    .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const { ValidateInputs } = require('./inputs');
+const { spawn } = require('child_process');
 const { Cleanup } = require('./post');
 const core = require('@actions/core');
-const exec = require('@actions/exec');
+const path = require('path');
+const fs = require('fs').promises;
 
 const IS_POST = !!core.getState('isPost');
 
@@ -11,20 +13,40 @@ const main = async () => {
             core.saveState('isPost', true);
             const [editor, args] = await ValidateInputs();
             const editorPath = process.platform === 'win32' ? `"${editor}"` : editor;
-            const exitCode = await exec.exec(editorPath, args, {
-                listeners: {
-                    stdout: (data) => {
-                        core.info(data.toString());
-                    },
-                    stderr: (data) => {
-                        core.info(data.toString());
-                    }
-                }
+            core.info(`[command]${editorPath} ${args.join(' ')}`);
+            const unityProcess = spawn(editorPath, args);
+            const unityPid = unityProcess.pid;
+            core.saveState('unityPid', unityPid);
+            // write pid to text file in workspace named 'unity-process-id.txt'
+            const pidFilePath = path.join(process.env.GITHUB_WORKSPACE, 'unity-process-id.txt');
+            await fs.writeFile(pidFilePath, unityPid.toString());
+            unityProcess.stdout.on('data', (data) => {
+                core.info(data.toString());
             });
-            if (exitCode !== 0) {
-                throw Error(`Unity failed with exit code ${exitCode}`);
-            }
+            unityProcess.stderr.on('data', (data) => {
+                core.error(data.toString());
+            });
+            await new Promise((resolve, reject) => {
+                unityProcess.on('close', (code) => {
+                    if (code === 0) {
+                        resolve();
+                    } else {
+                        reject(new Error(`Unity exited with code ${code}`));
+                    }
+                });
+            });
         } else {
+            const unityPid = core.getState('unityPid');
+            core.info(`Killing Unity process with PID ${unityPid}...`);
+            try {
+                process.kill(unityPid);
+            } catch (error) {
+                if (error.code === 'ESRCH') {
+                    core.info(`No process with PID ${unityPid} found.`);
+                } else {
+                    core.setFailed(`Failed to kill Unity process: ${error.message}`);
+                }
+            }
             await Cleanup();
         }
     } catch (error) {
