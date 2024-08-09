a8c390ec44558bde442fff673c0506c42e0d1324
Stephen Hodgson <hodgson.designs@gmail.com>
2024-08-08 20:41:08 -0400

    .

--- src/unity.js
+++ src/unity.js
@@ -1,21 +1,19 @@
 const core = require('@actions/core');
-const exec = require('@actions/exec');
 const { spawn } = require('child_process');
-const process = require('process');
 
 async function ExecUnity(editor, args) {
-    const process = spawn(`"${editor}"`, args, {
+    const unityProcess = spawn(`"${editor}"`, args, {
         stdio: ['pipe', 'pipe', 'pipe'],
         windowsVerbatimArguments: process.platform === `win32`
     });
-    process.stdout.on('data', (data) => {
+    unityProcess.stdout.on('data', (data) => {
         core.info(data.toString());
     });
-    process.stderr.on('data', (data) => {
+    unityProcess.stderr.on('data', (data) => {
         core.error(data.toString());
     });
     return new Promise((resolve, reject) => {
-        process.on('close', (code) => {
+        unityProcess.on('close', (code) => {
             resolve(code);
         });
     });
